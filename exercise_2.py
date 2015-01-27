49b167f8adbd554ee29d988f153107378764e549
Eunice Choi <euniceschoi93@gmail.com>
2015-01-27 08:48:08 +0000

    Complementing DNA

---
+++ exercise_2.py
@@ -0,0 +1,12 @@
+my_dna = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+#replace A with T
+replace1 = my_dna.replace ('A', 't')
+#replace T with A
+replace2 = my_dna.replace ('T', 'a')
+#replace C with G
+replace3 = my_dna.replace ('C', 'g')
+#replace G with C
+replace4 = my_dna.replace ('G', 'c')
+
+print (replace4)
+print (replace4.upper())
